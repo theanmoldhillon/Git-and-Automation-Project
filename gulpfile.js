64859ce18b848c09bbbab1dd381f56254b2b7299
Anmol-Singh <anmoldhillon999@gmail.com>
2018-05-16 11:43:02 -0500

    ADDED GULP IMAGEMIN

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
 
 gulp.task('default', function(){
@@ -18,4 +19,13 @@ gulp.task('css', function(){
 .pipe(cleanCSS({compatibility: 'ie8'}))
 
     .pipe(gulp.dest("./dist/css/"))
+});
+
+
+gulp.task('images', function(){
+    gulp.src('./src/images/*')
+
+    .pipe(imagemin())
+    .pipe(gulp.dest('./dist/images'))
+
 });
