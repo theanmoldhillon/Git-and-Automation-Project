8ff49f4a4479179c4bf7ff66a7898d5c77463011
Anmol-Singh <anmoldhillon999@gmail.com>
2018-05-16 16:58:13 -0500

    ADDED GULP CONCAT

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
 const babel = require('gulp-babel');
+const concat = require('gulp-concat');
 
 
 gulp.task('default', function(){
@@ -37,6 +38,9 @@ gulp.task('js', function(){
     .pipe(babel({
         presets: ['env']
     }))
+
+    .pipe(concat('main.js'))
+
     .pipe(gulp.dest('./dist/js/'))
 
 });
